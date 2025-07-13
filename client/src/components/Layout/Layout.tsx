b1aff72e3e24072d8e5b0362abd3c95260f50cd2
devnish23 <devnish23@gmail.com>
2025-07-13 13:52:35 +0800

    UI Skelaton is ready

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC = () => {
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <div className="flex">
         <Sidebar />
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col md:ml-64">
           <Header />
           <main className="flex-1 p-6">
             <Outlet />
